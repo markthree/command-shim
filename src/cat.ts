dc5d60c4a71efed5a24879489d483b6d020b6e78
markthree <1801982702@qq.com>
2023-10-01 13:46:54 +0800

    perf(cat): use stream

--- src/cat.ts
+++ src/cat.ts
@@ -1,16 +1,14 @@
 import { exists } from "https://deno.land/std@0.203.0/fs/exists.ts";
 
-const file = Deno.args[0];
+const path = Deno.args[0];
 
 if (
-  await exists(file, {
+  await exists(path, {
     isFile: true,
   })
 ) {
-  const fileText = await Deno.readTextFile(file);
-  console.log();
-  console.log(fileText);
-  console.log();
+  const file = await Deno.open(path);
+  await file.readable.pipeTo(Deno.stdout.writable);
 } else {
-  console.log(`${file} is not exists`);
+  console.log(`${path} is not exists`);
 }
